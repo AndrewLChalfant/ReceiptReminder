cd5022467e8fe721cbfe11a6f754bb5fa9935fcf
Andrew Chalfant <andrewlchalfant@gmail.com>
2019-11-27 16:48:34 -0500

    graphing basics

--- app/src/main/java/com/example/receiptreminder/UserTrendsPage.java
+++ app/src/main/java/com/example/receiptreminder/UserTrendsPage.java
@@ -8,8 +8,28 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * User views user trends.
@@ -61,5 +81,87 @@ public class UserTrendsPage extends AppCompatActivity {
 
             }
         });
+        //setContentView(R.layout.activity_user_trends_page);
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        Cartesian cartesian = AnyChart.line();
+        cartesian.animation(true);
+
+        //SETUP GRAPH STYLING
+        cartesian.padding(10d, 20d, 5d, 20d);
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                // TODO ystroke
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("User Purchasing Habits");
+        cartesian.yAxis(0).title("Dollars Spent");
+        cartesian.xAxis(0).title("Date Purchased");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+        //CREATE DUMMY DATA
+        List<String> temp = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i=1; i <= 12; i++) {
+            String val = rand.nextInt(1000) + "";
+            temp.add(i + "/1," + val);
+        }
+        //CONVERT DATA POINTS
+        List<DataEntry> seriesData = new ArrayList<>();
+        for (String a : temp) {
+            String[] split = a.split(",");
+            String date = split[0];
+            Integer val = Integer.parseInt(split[1]);
+            seriesData.add(new CustomDataEntry(date, val));
+        }
+
+        //seriesData.add(new CustomDataEntry("2009", 12.0, 22.5, 8.9)); //extend for multiple lines
+
+        Set set = Set.instantiate();
+        set.data(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("Total Spending");
+
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+
+        cartesian.legend().enabled(true);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+        anyChartView.setChart(cartesian);
+
+    }
+
+    public void monthButton(View v) {
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        Toast.makeText(getApplicationContext(), "Month Click", Toast.LENGTH_SHORT).show();
+        anyChartView.getBaseline();
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+        CustomDataEntry(String x, Number value) {
+            super(x, value);
+        }
+
+        CustomDataEntry(String x, Number value, Number value2, Number value3) {
+            super(x, value);
+            setValue("value2", value2);
+            setValue("value3", value3);
+        }
+
     }
 }
